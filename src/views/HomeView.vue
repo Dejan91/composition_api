f75f6e0b7b3c970e84f75b001dcc588cccf4c8f8
Dejan Stankovic <dejan.stankovic@madduckcode.com>
2022-08-01 15:17:09 +0200

    Clean up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive, onMounted, toRefs, computed } from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+
   }
 }
 </script>
